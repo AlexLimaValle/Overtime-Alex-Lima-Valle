99570fbc81fd87137d1c8a2b3ca3646672b86ded
AlexLimaValle <alexlimavalle@gmail.com>
2022-11-11 07:40:29 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
-import Header from './components/Header'
-import './Header.css'
-import ItenListContainer from './components/ItemListContainer'
 
 function App() {
 
   return (
       <div className="main">
-          <Header/>
-          <ItenListContainer saludo="HOLA MUNDO"/>
+    
       </div>
   )
 }
--- src/components/ItemListContainer.jsx
+++
@@ -1,8 +0,0 @@
-function ItenListContainer({saludo}){
-    return (
-        <h1>{saludo}</h1>
-    )
-}
-
-
-export default ItenListContainer
